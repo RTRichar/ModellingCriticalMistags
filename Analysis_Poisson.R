6c5cf8b0b948d78bac723bfe86b17ea3ac543e4f
Rodney Richardson <richardson.827@osu.edu>
2022-01-03 11:54:23 -0500

    Update Analysis_Poisson.R

--- Analysis_Poisson.R
+++ Analysis_Poisson.R
@@ -1,8 +1,8 @@
 #####################################################################################################
-# Try it out on real data 
+# Try it out on simulated data
 
 # load the data
-df <- read.csv("~/Projects/CriticalMistagNB/Resubmission/SimulatedDataset.csv", row.names = 1)
+df <- read.csv("SimulatedDataset.csv", row.names = 1)
 
 CMs <- colSums(df[,31:40]) # list of mistags per control bin
 Samples <- 40 # total number of dual-index bins (experimental samples plus control samples)
